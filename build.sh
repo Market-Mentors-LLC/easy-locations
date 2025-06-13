8248b59fc321be1c68404ae239f9fb0d75d29872
Tyler Seabury <tylerseabury@gmail.com>
2025-06-13 12:20:50 -0400

    version bump

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Define the plugin name and version
 PLUGIN_NAME="easy-locations"
-VERSION="0.0.4"
+VERSION="0.0.5"
 
 # Define the build directory
 BUILD_DIR="build"
--- easy-locations.php
+++ easy-locations.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Easy Locations
  * Plugin URI:        https://https://github.com/Market-Mentors-LLC/easy-locations
  * Description:       A WordPress plugin that adds easy location management, listing, and maps functionality.
- * Version:           0.0.4
+ * Version:           0.0.5
  * Author:            Market Mentors, LLC.
  * Author URI:        https://marketmentors.com/
  * License:           GPL-2.0+
@@ -98,7 +98,7 @@ if (version_compare($min_wp_v, \get_bloginfo('version'), '>=')) {
  * Currently plugin version.
  * Start at version 0.0.1 and use SemVer - https://semver.org
  */
-define('EASY_LOCATIONS_VERSION', '0.0.4');
+define('EASY_LOCATIONS_VERSION', '0.0.5');
 define('EASY_LOCATIONS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 /**
