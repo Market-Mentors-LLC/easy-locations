d0af255287ed7580e8fd1efdc9f4723b7516d57a
devon <dldesi6n@gmail.com>
2025-07-08 12:41:54 -0400

    version bump

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit7eeb44eac56c367fbf8f3266befb9971
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit7eeb44eac56c367fbf8f3266befb9971::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire7eeb44eac56c367fbf8f3266befb9971($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit7eeb44eac56c367fbf8f3266befb9971::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire7eeb44eac56c367fbf8f3266befb9971($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
